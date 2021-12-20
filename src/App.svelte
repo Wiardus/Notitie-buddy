1079e7e17f12f0123bc13fc2173a10ed2717b1b9
Wiardus <ynte.braaksma@student.nhlstenden.com>
2021-12-20 15:18:02 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+	import Navbar from './Navbar.svelte'
+
+	let header = "Ynte's Svelte App";
+	let count = 0
+	const increment = () => {
+		count ++
+	}
+	const decrement = () => {
+		count --
+	}
+	let newTask = ''
+	let taskList = []
+
+	function addTask() {
+		taskList=[...taskList, {text: newTask, completed: false}]
+		if (newTask.length < 1) {
+			alert('Je moet wel iets invullen broer')
+			return
+		} else {
+			return newTask = ''
+		}
+	}
+
+	function deleteTask(index) {
+		taskList.splice(index, 1)
+		taskList = taskList
+	}
+
+</script>
+
+<Navbar />
+
+<h1>{header}!</h1>
+<button on:click={increment}>Increment</button>
+<button on:click={decrement}>Decrement</button>
+<p>{count}</p>
+
+<input bind:value={newTask} type="text" placeholder="wat moet je doen...">
+<button on:click={addTask}>Add Task</button>
+<br/>
+
+{#each taskList as task, index}
+<input bind:checked={task.completed} type="checkbox">
+<span class:completed={task.completed}>{task.text}</span>
+<span class=deleteBtn on:click={() => deleteTask(index)}>X</span>
+<br/>
+{/each}
+
+<style>
+	h1 {
+		color: darkgoldenrod;
+	}
+	.completed {
+		text-decoration: line-through;
+	}
+	.deleteBtn {
+		cursor:pointer;
+		margin-left: 100px;
+		border: 1px solid #ccc;
+	}
+</style>
---
+++ src/Navbar.svelte
@@ -0,0 +1,34 @@
+<script>
+    export const navbar = undefined
+    let open = false
+    function toggleOpen() {
+        if (!open) {
+            open = true
+        } else {
+            open = false;
+        }
+    }
+</script>
+
+<navbar>
+    <button>Home</button>
+    <button on:click={toggleOpen}>About</button>
+    {#if open}
+    <p class=about>Hoi</p>
+    {/if}
+</navbar>
+
+<style>
+    navbar {
+        background: gainsboro;
+        width: 100vw;
+        height: 5vh;
+        position: absolute;
+    }
+
+    .about {
+        width: 100px;
+        height: 100px;
+        background: gainsboro;
+    }
+</style>
